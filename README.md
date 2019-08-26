aa0576c0b8b3761b01c2d5c814610f196a389bf7
Dave Withnall <dave@kayosdesign.com>
2019-08-26 12:41:29 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# VVV Laravel site template
+For setting up Laravel on VVV boxen
+
+## Overview
+This template will allow you to create a WordPress dev environment using only `vvv-custom.yml`.
+
+The supported environments are:
+- A single site
+
+# Configuration
+
+### The minimum required configuration:
+
+```yaml
+my-site:
+  repo: https://github.com/blackdogdevelopers/vvv-vagrant
+  hosts:
+    - my-site.test
+```
---
+++ provision/vvv-init.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo " * Custom site template provisioner - downloads and installs a copy of Laravel latest for testing, building client sites, etc"
+
+# fetch the first host as the primary domain. If none is available, generate a default using the site name
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+echo -e "\nGranting the laravel user priviledges to the '${DB_NAME}' database"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO laravel@localhost IDENTIFIED BY 'laravel';"
+echo -e "\n DB operations done.\n\n"
+
+echo "Setting up the log subfolder for Nginx logs"
+noroot mkdir -p ${VVV_PATH_TO_SITE}/log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
+
+echo "Setting up the log subfolder for Nginx logs"
+noroot mkdir -p ${VVV_PATH_TO_SITE}/public_html
+
+composer create-project laravel/laravel ${VVV_PATH_TO_SITE}/public_html
+
+
+echo "Copying the sites Nginx config template ( fork this site template to customise the template )"
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+  echo "Inserting the SSL key locations into the sites Nginx config"
+  VVV_CERT_DIR="/srv/certificates"
+  # On VVV 2.x we don't have a /srv/certificates mount, so switch to /vagrant/certificates
+  codename=$(lsb_release --codename | cut -f2)
+  if [[ $codename == "trusty" ]]; then # VVV 2 uses Ubuntu 14 LTS trusty
+    VVV_CERT_DIR="/vagrant/certificates"
+  fi
+  sed -i "s#{{TLS_CERT}}#ssl_certificate ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  sed -i "s#{{TLS_KEY}}#ssl_certificate_key ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
+
+echo "Site Template provisioner script completed"
